9ff6cfbf424391b1a23b114b42a0ef2db6e30be0
tadod12 <dubstep.0509dtd@gmail.com>
2026-02-16 01:09:54 +0700

    feat: add robots.txt and sitemap.xml generation for SEO.

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,6 +2,8 @@ import { MetadataRoute } from 'next';
 import { getSortedPostsData } from '@/lib/posts';
 import { i18n } from '@/app/i18n';
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
     const baseUrl = 'https://dodat.dev'; // Replace with actual domain
 
